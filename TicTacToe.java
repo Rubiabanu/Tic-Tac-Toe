54a11131f8c2fe62ae1337b08ae9359aa71daf3b
Rubiabanu <152153543+Rubiabanu@users.noreply.github.com>
2023-11-27 13:04:07 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '}
+        };
+
+        char currentPlayer = 'X';
+        boolean gameWon = false;
+
+        while (!gameWon) {
+            printBoard(board);
+            makeMove(board, currentPlayer);
+            gameWon = checkWin(board, currentPlayer);
+            if (!gameWon) {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+
+        printBoard(board);
+        System.out.println("Player " + currentPlayer + " wins!");
+    }
+
+    private static void printBoard(char[][] board) {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    private static void makeMove(char[][] board, char currentPlayer) {
+        Scanner scanner = new Scanner(System.in);
+        int row, col;
+
+        do {
+            System.out.print("Player " + currentPlayer + ", enter your move (row and column): ");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+        } while (!isValidMove(board, row, col));
+
+        board[row][col] = currentPlayer;
+    }
+
+    private static boolean isValidMove(char[][] board, int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            System.out.println("Invalid move. Try again.");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkWin(char[][] board, char currentPlayer) {
+        // Check rows and columns
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+                (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+            (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+            return true;
+        }
+
+        // Check for a tie
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+
+        // The game is a tie
+        System.out.println("The game is a tie!");
+        System.exit(0);
+
+        return false;
+    }
+}
